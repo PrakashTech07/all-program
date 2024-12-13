76cd460cb49a055b85ac191a919eba3594dc7ffe
PrakashTech07 <prakash.eng.prof@gmail.com>
2024-12-13 17:14:09 +0530

    make a code better

--- gst.c
+++ gst.c
@@ -8,6 +8,9 @@ scanf("%f",&value);
 price(value);
     return 0;
 }
+
+
+
 void price(float value){// function defination with parameter type
     value =value+ (0.18 * value);
     printf("%f", value);
